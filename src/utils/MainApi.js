e0206a97d83f922dd4c00fd107dbe9300d737832
Evgeniy Dymov <krionic1@gmail.com>
2024-01-08 00:21:08 +0300

    изменяет адрес

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-const { REACT_APP_API_URL = 'http://localhost:3001' } = process.env;
+const { REACT_APP_API_URL = 'api.movie-explorer.dymov.nomoredomainsmonster.ru' } = process.env;
 
 class Api {
     constructor(options) {
